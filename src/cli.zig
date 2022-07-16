ecd057474a1e6ce3e2104ff9ee6d87696caf1fe2
Zach Gray <zach@flare.build>
2022-07-16 17:42:53 +0200

    rename

--- src/cli.zig
+++ src/cli.zig
@@ -3,11 +3,11 @@ const std = @import("std");
 
 const io = std.io;
 
-pub const Fs3Args = struct {
+pub const GrpcExampleArgs = struct {
     port: u16,
 };
 
-pub fn parseArgs() !*Fs3Args {
+pub fn parseArgs() !*GrpcExampleArgs {
     const params = comptime clap.parseParamsComptime(
         \\-p, --port <u16>   Port to listen on.
     );
@@ -19,7 +19,7 @@ pub fn parseArgs() !*Fs3Args {
         return err;
     };
     defer res.deinit();
-    var args = Fs3Args{
+    var args = GrpcExampleArgs{
         .port = 3000,
     };
     if (res.args.port) |n|
